344d734d74685241865357ee0e2d8fb89c7ec49a
Sebastian Consuegra <sebastian951810@gmail.com>
2024-04-16 10:26:59 -0500

    Update

---
+++ practica_8/modules/s3/output.tf
@@ -0,0 +1,3 @@
+output "s3_srn" {
+  value = aws_s3_bucket.cerverus_bucket.arn
+}
--- practica_8/vpc.tf
+++ practica_8/vpc.tf
@@ -83,3 +83,30 @@ module "mybucket" {
   bucket_name = local.s3-sufix
 }
 
+output "s3_srn" {
+  value = module.mybucket.s3_srn
+}
+
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+module "terraform_state_backend" {
+  source = "cloudposse/tfstate-backend/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version     = "x.x.x"
+  namespace  = "eg"
+  stage      = "test"
+  name       = "terraform"
+  attributes = ["state"]
+
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
+}
+
+# Your Terraform configuration
+module "another_module" {
+  source = "....."
+}
+
